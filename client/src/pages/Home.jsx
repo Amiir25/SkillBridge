429160d1537fe1b58cb15a1a28b2b6def4b79057
Amiir25 <amirsadik636@gmail.com>
2025-10-06 10:00:01 +0300

    Add styles and animations on the titles in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,9 +13,13 @@ const Home = () => {
         shadow-xl shadow-gray-800/30' />
 
         {/* Titles */}
-        <div className='border h-screen flex flex-col items-center justify-center'>
-          <h1>Welcome to the real world of skills & experiance!</h1>
-          <p>Connecting students with short, practical projects from companies</p>
+        <div id='titles' className='h-screen w-200 mx-auto flex flex-col justify-center text-center text-gray-900'>
+          <h1 className='text-6xl font-semibold tex-shadow-2xl [text-shadow:4px_4px_12px_rgba(0,0,0,0.8)]'>
+            Welcome to the real world of skills & experiance!
+          </h1>
+          <p className='text-2xl mt-8'>
+            Connecting students with short, practical projects from companies
+          </p>
         </div>
     </div>
   )
