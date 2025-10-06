7d2fdfa7c297694918ee1502e2d725b20673efc5
Amiir25 <amirsadik636@gmail.com>
2025-10-06 10:58:51 +0300

    Add buttons on the home page and update the height of the div in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { images } from '../assets/images'
 
 const Home = () => {
   return (
-    <div
+    <section
     style={{ backgroundImage: `url(${ images.bg })` }}
     className='relative h-screen bg-cover bg-center bg-no-repeat'>
 
@@ -12,16 +12,31 @@ const Home = () => {
         className='absolute top-4 left-4 w-40 ml-6 md:ml-12 lg:ml-24 xl:ml-32
         shadow-xl shadow-gray-800/30' />
 
-        {/* Titles */}
-        <div id='titles' className='h-screen w-200 mx-auto flex flex-col justify-center text-center text-gray-900'>
-          <h1 className='text-6xl font-semibold tex-shadow-2xl [text-shadow:4px_4px_12px_rgba(0,0,0,0.8)]'>
-            Welcome to the real world of skills & experiance!
-          </h1>
-          <p className='text-2xl mt-8'>
-            Connecting students with short, practical projects from companies
-          </p>
+        <div id='hero' className='h-[95vh] w-200 mx-auto flex flex-col justify-center text-center'>
+          {/* Titles */}
+          <div className=' text-gray-900'>
+            <h1 className='text-6xl font-semibold tex-shadow-2xl [text-shadow:4px_4px_12px_rgba(0,0,0,0.8)]'>
+              Welcome to the real world of skills & experiance!
+            </h1>
+            <p className='text-2xl mt-8'>
+              Connecting students with short, practical projects from companies
+            </p>
+          </div>
+
+          {/* Buttons */}
+          <div className='mt-10'>
+            <button className='mr-10 text-2xl border px-8 py-2 rounded cursor-pointer
+            hover:bg-gray-900 hover:text-gray-100 transition-all duration-200'>
+              Login
+            </button>
+
+            <button className='text-2xl border px-8 py-2 rounded cursor-pointer
+            hover:bg-gray-900 hover:text-gray-100 transition-all duration-200'>
+              Register
+            </button>
+          </div>
         </div>
-    </div>
+    </section>
   )
 }
 
