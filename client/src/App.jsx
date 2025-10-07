246577fe093c65e54cc5d1143c0d8adaf9d4edba
Amiir25 <amirsadik636@gmail.com>
2025-10-07 21:29:16 +0300

    Create routes for pages in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './company/Dashboard'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={ <Home/> } />
+      <Route path='/login' element={ <Login/> } />
+      <Route path='/register' element={ <Register/> } />
+
+      {/* Company routes */}
+      <Route path='/company/dashboard' element={ <Dashboard/> } />
     </Routes>
   )
 }
