42f1a470a1d255bd86d4ed28cd08566c3be8331a
Amiir25 <amirsadik636@gmail.com>
2025-11-06 08:07:41 +0300

    Add server.js

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { connectDB } from './config/db.js';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// DB
+connectDB();
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('API working');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+})
