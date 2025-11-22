d1e9b67b79040f4fe70987a1e9b1ab4f702c8ad4
Amiir25 <amirsadik636@gmail.com>
2025-11-22 14:41:29 +0300

    Add authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ export const authMiddleware = async (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(payload.id).select('-password');
         if (!user) {
             return res.status(401).json({ success: false, message: 'User not found' });
