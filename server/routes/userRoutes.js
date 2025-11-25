0ad72dc4cfaeed2db9de821b8c91e12a35190e7d
Amiir25 <amirsadik636@gmail.com>
2025-11-25 22:07:03 +0300

    Rebuild the server

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { login, register } from '../controllers/userController.js';
-
-const userRouter = Router();
-
-userRouter.post('/register', register);
-userRouter.post('/login', login);
-
-export default userRouter;
