ff4628bf767c29e9d89d29b17e1d6f7c58a2b30f
Amiir25 <amirsadik636@gmail.com>
2025-11-26 15:11:07 +0300

    Create a login controller function in authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
+// Register
 export const registerUser = async (req, res) => {
     try {
         const { name, email, password, role } = req.body;
@@ -41,5 +43,58 @@ export const registerUser = async (req, res) => {
     } catch (error) {
         console.error('Registeration Error:', error.message);
         return res.status(500).json({ message: 'Server error while registration' });
+    }
+}
+
+// Login
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Verify fields
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' });
+        }
+
+        // Find user
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Email not found' });
+        }
+
+        // Match password
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: 'Incorrect password' });
+        }
+
+        // jwt payload
+        const payload = {
+            id: user._id,
+            email: user.email,
+            role: user.role,
+        }
+
+        // Create jwt
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.TOKEN_EXPIRES_IN },
+        )
+
+        return res.status(201).json({
+            message: 'Login successful',
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            }
+        })
+        
+    } catch (error) {
+        console.error('Login Error:', error.message);
+        return res.status(500).json({ message: 'Server Error' });
     }
 }
