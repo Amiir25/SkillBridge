5dab45f017cabef50edffa1e5f88de0cc1abfc0b
Amiir25 <amirsadik636@gmail.com>
2025-11-26 13:45:01 +0300

    Remove the 6 digit restriction from password field in userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type:String,
         required: true,
-        maxlength: 6,
+        // maxlength: 6,
     },
     role: {
         type: String,
