471c892857ce319d2e295efc8470586e28c15970
Amiir25 <amirsadik636@gmail.com>
2025-11-26 07:36:23 +0300

    Create a server in server.js

--- server/server.js
+++ server/server.js
@@ -1,29 +1,11 @@
 import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { connectDB } from './config/db.js';
-import userRouter from './routes/userRoutes.js';
-import projectRouter from './routes/projectRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-
-// Middlewares
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-
-// DB
-connectDB();
-
-// Routes
-app.use('/api/auth', userRouter);
-app.use('/api/projects', projectRouter);
 
 app.get('/', (req, res) => {
-    res.send('API working');
+    res.send('Welcome to SkillBridge');
 })
 
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+app.listen(4000, () => {
+    console.log('Server started on http://localhost:4000');
 })
