7c512d3995288438b359246a4cb946d0468f66b1
Amiir25 <amirsadik636@gmail.com>
2025-11-27 07:59:11 +0300

    Add a route for project update in projectRoutes.js

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { middleware } from '../middleware/authMiddleware.js';
-import { createProject, getAllProjects, projectDetail } from '../controllers/projectController.js';
+import { createProject, getAllProjects, projectDetail, updateProject } from '../controllers/projectController.js';
 
 const projectRouter = Router();
 
@@ -10,5 +10,6 @@ projectRouter.get('/project-detail/:id', projectDetail);
 
 // Protected routes
 projectRouter.post('/create-project', middleware, createProject);
+projectRouter.put('/update-project/:id', middleware, updateProject);
 
 export default projectRouter;
