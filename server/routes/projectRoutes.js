efe48b67e366a592ab7b64e11d53c76e511f473c
Amiir25 <amirsadik636@gmail.com>
2025-11-27 07:28:05 +0300

    Create a route for project detail controller in projectRoutes.js

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { middleware } from '../middleware/authMiddleware.js';
-import { createProject } from '../controllers/projectController.js';
+import { createProject, getAllProjects, projectDetail } from '../controllers/projectController.js';
 
 const projectRouter = Router();
 
+// Public routes
+projectRouter.get('/', getAllProjects);
+projectRouter.get('/project-detail/:id', projectDetail);
+
+// Protected routes
 projectRouter.post('/create-project', middleware, createProject);
 
 export default projectRouter;
