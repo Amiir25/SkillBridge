0e6cf30479a9f3dfc5fab4e29b656b83baa433b9
Amiir25 <amirsadik636@gmail.com>
2025-11-27 15:23:54 +0300

    Add a route for company dashboard in applicationRoutes.js

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { middleware } from "../middleware/authMiddleware.js";
-import { applyToProject } from "../controllers/applicationController.js";
+import { applyToProject, compnayDashboard } from "../controllers/applicationController.js";
 
 const applicationRouter = Router();
 
 applicationRouter.post('/apply/:projectId', middleware, applyToProject);
+applicationRouter.get('/company/dashboard', middleware, compnayDashboard);
 
 export default applicationRouter;
