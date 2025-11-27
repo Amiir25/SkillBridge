62ed000e809ee4a47afe68d51138284ab55c0e25
Amiir25 <amirsadik636@gmail.com>
2025-11-27 12:14:30 +0300

    Create a delete controller function in projectController.js

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -151,3 +151,34 @@ export const updateProject = async (req, res) => {
         return res.status(500).json({ message: 'Server error' });
     }
 }
+
+// Delete project
+export const deleteProject = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Ensure only companies can update projects
+        if (req.user.role !== 'Company') {
+            return res.status(403).json({ message: 'Only companies can delete projects' })
+        }
+
+        // Check if project exists & belongs to the company
+        const existingProject = await Project.findById(id);
+        if (!existingProject) {
+            return res.status(404).json({ message: 'Project not found' });
+        }
+
+        if (existingProject.companyId.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: 'You can only delete your own projects' });
+        }
+
+        // Delete project
+        await Project.findByIdAndDelete(id);
+
+        return res.status(200).json({ message: 'Project deleted successfully' });
+
+    } catch (error) {
+        console.error('Error while deleting project:', error.message);
+        return res.status(500).json({ message: 'Server error' });
+    }
+}
