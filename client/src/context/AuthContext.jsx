d2ff7ca578e425d92be59558dfffb8b2b948602e
Amiir25 <amirsadik636@gmail.com>
2025-11-30 06:49:49 +0300

    Create AuthContext.jsx

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import api from "../utils/axiosConfig";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Fetch logged-in user info (using cookie)
+    const fetchUser = async () => {
+        try {
+            const res = await api.get('/auth/me');
+            setUser(res.data.user);
+        } catch {
+            setUser(null);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    // Login
+    const login = async (data) => {
+        const res = await api.post('/auth/login', data);
+        await fetchUser(); // refresh context with user data
+        return res.data;
+    };
+
+    // Logout
+    const logout = async () => {
+        await api.post('auth/logout');
+        setUser(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout }}>
+            { children }
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = () => useContext(AuthContext);
