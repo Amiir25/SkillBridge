bd05450d648efda76063a4de1a07d3768f2bf0b0
Amiir25 <amirsadik636@gmail.com>
2025-11-30 06:58:21 +0300

    Add a route to get logged-in user based on the JWT cookie in authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { loginUser, registerUser } from '../controllers/authController.js';
+import { middleware } from '../middleware/authMiddleware.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
+// Get logged-in user based on the JWT cookie
+authRouter.get('/me', middleware , async (req, res) => {
+    res.status(200).json({ user: req.user });
+})
 
 export default authRouter;
