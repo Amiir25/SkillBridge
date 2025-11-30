b87afb9a3d0b2efd444599619a6f63e2c682f4ca
Amiir25 <amirsadik636@gmail.com>
2025-11-30 07:56:24 +0300

    Add logout route in authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import { loginUser, logout, registerUser } from '../controllers/authController.js';
 import { middleware } from '../middleware/authMiddleware.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
+authRouter.post('/logout', logout);
 // Get logged-in user based on the JWT cookie
 authRouter.get('/me', middleware , async (req, res) => {
     res.status(200).json({ user: req.user });
