de5f1880061f8ae1d484e7e3cce5ca7ae5190ac1
Amiir25 <amirsadik636@gmail.com>
2025-11-30 07:50:08 +0300

    Add a route for company dashboard in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
+import RoleRoute from './components/RoleRoute';
 
 function App() {
   return (
@@ -9,6 +10,13 @@ function App() {
       <Route path='/' element={ <Home/> } />
       <Route path='/login' element={ <Login/> } />
       <Route path='/register' element={ <Register/> } />
+
+      {/* Company dashboard */}
+      <Route path='/company/dashboard' element={
+        <RoleRoute role='Company'>
+          <CompanyDashboard/>
+        </RoleRoute>
+      } />
     </Routes>
   )
 }
