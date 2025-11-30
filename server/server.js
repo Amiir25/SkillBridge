c05fa8110e2e2ea40d85e82251a5113174311637
Amiir25 <amirsadik636@gmail.com>
2025-11-30 20:28:27 +0300

    Add cookie-parsers in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import authRouter from './routes/authRoutes.js';
 import projectRouter from './routes/projectRoutes.js';
@@ -13,6 +14,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cookieParser());
 
 // Routes
 app.use('/api/auth', authRouter);
