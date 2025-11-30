b9fce5225ea4cae0f3249ef9f7b61bbcd0254e43
Amiir25 <amirsadik636@gmail.com>
2025-11-30 20:27:56 +0300

    Update middleware to check jwt in cookie in authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,26 +4,25 @@ import User from '../models/userModel.js';
 export const middleware = async (req, res, next) => {
     let token;
 
-    // Check for authorization header
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        try {
-            // Extract token
-            token = req.headers.authorization.split(' ')[1];
-
-            // Verify token
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-            // Attach user object
-            req.user = await User.findById(decoded.id).select('-password');
-
-            return next();
-
-        } catch (error) {
-            console.error('Authentication Error:', error.message);
-            return res.status(401).json({ message: 'Not authorized, token failed' });
-        }
+    // Check for token in cookies
+    if (req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }
+    // Fallback: Check for token in Authorization header
+    else if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
     }
 
-    // Not token provided
-    return res.status(401).json({ message: 'Not authorized, no token provided' });
+    if (!token) {
+        return res.status(401).json({ message: 'Not authorized, no token provided' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password');
+        return next();
+    } catch (error) {
+        console.error('Authentication Error:', error.message);
+        return res.status(401).json({ message: 'Not authorized, token failed' });
+    }
 }
