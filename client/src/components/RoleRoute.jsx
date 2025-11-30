7b04e7290bf6b5c51fa0d768000c01516b9fd01b
Amiir25 <amirsadik636@gmail.com>
2025-11-30 07:23:39 +0300

    Add RoleRoute.jsx

---
+++ client/src/components/RoleRoute.jsx
@@ -0,0 +1,16 @@
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+const RoleRoute = ({ children, role }) => {
+    const { user, loading } = useAuth();
+
+    if (loading) return <p>Loading...</p>
+
+    if (!user) return <Navigate to={'/login'} />
+
+    if (user.role !== role) return <Navigate to={'/'} />
+
+    return children;
+}
+
+export default RoleRoute
