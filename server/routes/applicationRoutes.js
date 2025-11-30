7ec81b79ae8d6754acbe015efca7201ac92654c6
Amiir25 <amirsadik636@gmail.com>
2025-11-30 07:55:21 +0300

    Fix typo error in applicationRoutes.js

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { middleware } from "../middleware/authMiddleware.js";
-import { applyToProject, compnayDashboard, studentDashboard } from "../controllers/applicationController.js";
+import { applyToProject, companyDashboard, studentDashboard } from "../controllers/applicationController.js";
 
 const applicationRouter = Router();
 
 applicationRouter.post('/apply/:projectId', middleware, applyToProject);
-applicationRouter.get('/company/dashboard', middleware, compnayDashboard);
+applicationRouter.get('/company/dashboard', middleware, companyDashboard);
 applicationRouter.get('/student/dashboard', middleware, studentDashboard);
 
 export default applicationRouter;
