57e502df9e0bdfd3e3d77a974444addfe85685fb
Amiir25 <amirsadik636@gmail.com>
2025-12-02 17:36:23 +0300

    Add a route forstudent  profile checker controller in profileRoutes.js

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { middleware } from "../middleware/authMiddleware.js";
-import { studentProfile } from "../controllers/studentProfileController.js";
+import { checkStudentProfile, studentProfile } from "../controllers/studentProfileController.js";
 
 const profileRouter = Router();
 
+profileRouter.get('/student/me', middleware, checkStudentProfile);
 profileRouter.post('/student/profile-setup', middleware, studentProfile);
 
 export default profileRouter;
