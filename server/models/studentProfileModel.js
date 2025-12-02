13a7346a4e3e68a51c7088f97b7d4d7fa8cc6180
Amiir25 <amirsadik636@gmail.com>
2025-12-02 15:40:31 +0300

    Create a databse model for student profile in studentProfileModel.js

---
+++ server/models/studentProfileModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const studentProfileSchema = new mongoose.Schema({
+    studentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true,
+    },
+    firstName: { type: String, required: true,trim: true, },
+    lastName: { type: String, required: true,trim: true, },
+    address: { type: String, required: true,trim: true, },
+    university: { type: String, required: true,trim: true, },
+    major: { type: String, required: true,trim: true, },
+    year: { 
+        type: String,
+        enum: ['1st year', '2nd year', '3rd year', '4th year', '5th year'],
+        default: 'Not mentioned',
+    },
+
+    // Optional fields
+    skills: { type: [String], },
+    bio: { type: String, },
+    portfolioLink: { type: String,},
+    resumeLink: { type: String, },
+
+}, { timestamps: true });
+
+const StudentProfile = mongoose.model('StudentProfile', studentProfileSchema);
+export default StudentProfile;
