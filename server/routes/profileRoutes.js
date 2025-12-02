884cd879097b52ddb8fccf79364c1351194f12a4
Amiir25 <amirsadik636@gmail.com>
2025-12-02 20:28:06 +0300

    Add a for company profile in profileRoutes.js

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { middleware } from "../middleware/authMiddleware.js";
-import { checkStudentProfile, studentProfile } from "../controllers/studentProfileController.js";
+import { checkStudentProfile, createStudentProfile } from "../controllers/profileControllers/studentProfileController.js";
+import { checkCompanyProfile, createCompanyProfile } from "../controllers/profileControllers/companyProfileController.js";
+
 
 const profileRouter = Router();
 
+// Student
 profileRouter.get('/student/me', middleware, checkStudentProfile);
-profileRouter.post('/student/profile-setup', middleware, studentProfile);
+profileRouter.post('/student/profile-setup', middleware, createStudentProfile);
+
+// Company
+profileRouter.get('/company/me', middleware, checkCompanyProfile);
+profileRouter.post('/company/profile-setup', middleware, createCompanyProfile);
 
 export default profileRouter;
