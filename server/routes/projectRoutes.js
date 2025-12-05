9c84ca2f0e2d6222978fd92791e529a756d59767
Amiir25 <amirsadik636@gmail.com>
2025-12-05 19:32:52 +0300

    Add new routes for close and manage project controllers in projectRoutes.js

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { middleware } from '../middleware/authMiddleware.js';
-import { createProject, deleteProject, getAllProjects, projectDetail, updateProject } from '../controllers/projectController.js';
+import {
+    closeProject, createProject, deleteProject, getAllProjects, manageProjects,
+    projectDetail, updateProject
+} from '../controllers/projectController.js';
 
 const projectRouter = Router();
 
@@ -10,7 +13,9 @@ projectRouter.get('/project-detail/:id', projectDetail);
 
 // Protected routes
 projectRouter.post('/create-project', middleware, createProject);
+projectRouter.put('/close-project/:id', middleware, closeProject);
 projectRouter.put('/update-project/:id', middleware, updateProject);
 projectRouter.delete('/delete-project/:id', middleware, deleteProject);
+projectRouter.get('/manage-projects', middleware, manageProjects);
 
 export default projectRouter;
