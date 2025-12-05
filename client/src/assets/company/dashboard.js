fdf4a58adb2f0b8391355babab2c1fde5bffc112
Amiir25 <amirsadik636@gmail.com>
2025-12-05 19:24:24 +0300

    Create company asset folder to store company data in  assets/company/dashboard.js

---
+++ client/src/assets/company/dashboard.js
@@ -0,0 +1,7 @@
+// Sidebar buttons on company dashboard
+export const sidebarBtns = [
+    { name: 'Dashboard', id: 'dashboard', path: '/company/dashboard' },
+    { name: 'Manage Projects', id: 'manage-projects', path: '/company/manage-projects' },
+    { name: 'Profile', id: 'profile', path: '/company/profile' },
+    { name: 'Home', id: 'home', path: '/' },
+];
