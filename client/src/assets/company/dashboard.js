6fb5e35b754e7a6cb0a311476afe8244d29f1144
Amiir25 <amirsadik636@gmail.com>
2025-12-07 10:08:32 +0300

    Move dashboard.js file to from /assets/dashboard to /assets folder

--- client/src/assets/company/dashboard.js
+++
@@ -1,7 +0,0 @@
-// Sidebar buttons on company dashboard
-export const sidebarBtns = [
-    { name: 'Dashboard', id: 'dashboard', path: '/company/dashboard' },
-    { name: 'Manage Projects', id: 'manage-projects', path: '/company/manage-projects' },
-    { name: 'Profile', id: 'profile', path: '/company/profile' },
-    { name: 'Home', id: 'home', path: '/' },
-];
---
+++ client/src/assets/dashboard.js
@@ -0,0 +1,15 @@
+// Sidebar buttons on company dashboard
+export const companyDashboard = [
+    { name: 'Dashboard', id: 'dashboard', path: '/company/dashboard' },
+    { name: 'Manage Projects', id: 'manage-projects', path: '/company/manage-projects' },
+    { name: 'Profile', id: 'profile', path: '/company/profile' },
+    { name: 'Home', id: 'home', path: '/' },
+];
+
+// Sidebar buttons on student dashboard
+export const studentDashboard = [
+    { name: 'Dashboard', id: 'dashboard', path: '/student/dashboard' },
+    { name: 'Applications', id: 'applications', path: '/student/applications' },
+    { name: 'Profile', id: 'profile', path: '/student/profile' },
+    { name: 'Home', id: 'home', path: '/' },
+];
