c4fa8fcfbcb964046c1f128391367caff5241f66
Amiir25 <amirsadik636@gmail.com>
2025-12-07 19:25:12 +0300

    Remove the route for manage project controller in projectRoutes.js

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { middleware } from '../middleware/authMiddleware.js';
 import {
-    closeProject, createProject, deleteProject, getAllProjects, manageProjects,
+    closeProject, createProject, deleteProject, getAllProjects,
     projectDetail, updateProject
 } from '../controllers/projectController.js';
 
@@ -16,6 +16,6 @@ projectRouter.post('/create-project', middleware, createProject);
 projectRouter.put('/close-project/:id', middleware, closeProject);
 projectRouter.put('/update-project/:id', middleware, updateProject);
 projectRouter.delete('/delete-project/:id', middleware, deleteProject);
-projectRouter.get('/manage-projects', middleware, manageProjects);
+// projectRouter.get('/manage-projects', middleware, manageProjects);
 
 export default projectRouter;
